6a82d10b65d8a31ecca7df2350b008852b6e42e6
Dillon-Yarduk <dillonyarduk@gmail.com>
2021-10-25 10:58:02 -0500

    Removed unnecessary scaffolded stuff.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :books
-  resources :publishers
-  resources :genres
-  resources :authors
+  resources :books, only: %i[index show]
+  resources :publishers, only: %i[index show]
+  resources :genres, only: %i[index show]
+  resources :authors, only: %i[index show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
