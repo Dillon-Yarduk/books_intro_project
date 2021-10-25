82d4cd7a939924b62241c571c8c873debf181e24
Dillon-Yarduk <dillonyarduk@gmail.com>
2021-10-25 03:17:03 -0500

    Fixed validation for books

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,5 @@
 class Book < ApplicationRecord
   belongs_to :publisher
   belongs_to :author
+  validates :title, :description, :page_count, :published_date, :publisher, :author, presence: true
 end
