87fcbc8b670da2e72d8ad7558e3d0701f609a26d
Dillon-Yarduk <dillonyarduk@gmail.com>
2021-10-28 20:25:09 -0500

    Got rid of un-needed stuff for pages.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_25_091147) do
+ActiveRecord::Schema.define(version: 2021_10_29_010430) do
 
   create_table "authors", force: :cascade do |t|
     t.string "name"
@@ -42,6 +42,14 @@ ActiveRecord::Schema.define(version: 2021_10_25_091147) do
 
   create_table "genres", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "pages", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.string "permalink"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
