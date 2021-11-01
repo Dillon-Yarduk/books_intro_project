7a20a14a1d7c70958da028f4c6f7b0f77c1d5202
Dillon-Yarduk <dillonyarduk@gmail.com>
2021-11-01 11:11:29 -0500

    Added second data source

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,4 +1,5 @@
 class Author < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   has_many :books
+  has_many :publishers, through: :books
 end
--- app/models/publisher.rb
+++ app/models/publisher.rb
@@ -1,4 +1,5 @@
 class Publisher < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   has_many :books
+  has_many :authors, through: :books
 end
